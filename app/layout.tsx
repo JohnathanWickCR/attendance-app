a36bbcba97e195aa52ed6f6085e134313a3c529f
JohnathanWickCR <hieusonglo1@gmail.com>
2026-04-02 16:06:57 +0700

    edit title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 import MainNav from '@/components/main-nav';
 
 export const metadata = {
-  title: 'Attendance App',
+  title: 'SLB Workers',
   description: 'Chấm công và tính chi phí nhân công theo dự án',
 };
 
